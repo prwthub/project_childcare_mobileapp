f1cd3c073ad2152733ed4fb8620ac7429e6850ac
prwt <perawit37@gmail.com>
2024-02-27 00:28:40 +0700

    v1.1

--- perforkid/web/perforkid/util/admin.js
+++ perforkid/web/perforkid/util/admin.js
@@ -1,6 +1,6 @@
 var admin = require("firebase-admin");
 
-var serviceAccount = require("./perforkid-application-firebase-adminsdk-5qzkc-84e1ad7149.json");
+var serviceAccount = require("./perforkid.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
