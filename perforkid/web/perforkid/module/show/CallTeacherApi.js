99d40dbc07e38498d6bc8b71751f7005a7da9b8a
prwt <perawit37@gmail.com>
2024-04-07 15:02:02 +0700

    Update CallTeacherApi.js

--- perforkid/web/perforkid/module/show/CallTeacherApi.js
+++ perforkid/web/perforkid/module/show/CallTeacherApi.js
@@ -1,4 +1,4 @@
-import { getStorage, ref, uploadBytes, listAll, getDownloadURL } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-storage.js";
+import { getStorage, ref, uploadBytes, listAll, getDownloadURL, deleteObject } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-storage.js";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
 
 const firebaseConfig = {
@@ -35,6 +35,19 @@ async function fetchTeacher() {
 }
 
 async function generateTeacherTable() {
+    
+    var itemsData = [];
+    const storageRef = ref(storage, `school/${currentUser.school_name}/teacher-images`);
+    try {
+        var { items } = await listAll(storageRef);
+        for (const itemRef of items) {
+            itemsData.push( itemRef.name );
+        }
+    } catch (error) {
+        console.error('Error listing files:', error);
+    }
+    console.log("itemsData : ",itemsData);
+    
     let tableHtml = `
     <table>
         <tr>
@@ -49,25 +62,11 @@ async function generateTeacherTable() {
             <th>upload image</th>
         </tr>
     `;
-
-    var itemsData = [];
-    const storageRef = ref(storage, `school/${currentUser.school_name}/teacher-images`);
-    try {
-        var { items } = await listAll(storageRef);
-        for (const itemRef of items) {
-            itemsData.push( itemRef.name );
-        }
-    } catch (error) {
-        console.error('Error listing files:', error);
-    }
-
-    console.log(itemsData);
     
     let schoolData = await fetchTeacher();
     for (const school of schoolData) {
-        const foundPNG = itemsData.includes(`${school["teacher-ID"]}.png`);
-        const foundJPG = itemsData.includes(`${school["teacher-ID"]}.jpg`);
-        if (foundPNG) {
+        const foundImage = itemsData.includes(`${school["teacher-ID"]}.png`);
+        if (foundImage) {
             const teacherImageRef = ref(storage, `school/${currentUser.school_name}/teacher-images/${school["teacher-ID"]}.png`);
             try {
                 const imageUrl = await getDownloadURL(teacherImageRef);
@@ -81,32 +80,15 @@ async function generateTeacherTable() {
                     <td>${school["subject"]}</td>
                     <td>${school["email"]}</td>
                     <td>${school["address"]}</td>
-                    <td><img id="previewImg-${school["teacher-ID"]}" src="${imageUrl}" style="height:100px"></td>
+                    <td style="text-align: center;">
+                        <img id="previewImg-${school["teacher-ID"]}" src="${imageUrl}" style="height:100px">
+                        <button id="delFile-${school["teacher-ID"]}"> Delete </button>
+                    </td>
                     <td><input id="fileInput-${school["teacher-ID"]}" type="file" accept="image/*"></td>
                 </tr>`;
             } catch (error) {
                 console.error('Error getting download URL:', error);
             }
-        } else if (foundJPG) {
-            const teacherImageRef = ref(storage, `school/${currentUser.school_name}/teacher-images/${school["teacher-ID"]}.jpg`);
-            try {
-                const imageUrl = await getDownloadURL(teacherImageRef);
-                console.log(`${school["teacher-ID"]} teacherImage exists`);
-                tableHtml += `
-                <tr>
-                    <td>${school["teacher-ID"]}</td>
-                    <td>${school["name-surname"]}</td>
-                    <td>${school["class-room"]}</td>
-                    <td>${school["teaching-room"]}</td>
-                    <td>${school["subject"]}</td>
-                    <td>${school["email"]}</td>
-                    <td>${school["address"]}</td>
-                    <td><img id="previewImg-${school["teacher-ID"]}" src="${imageUrl}" style="height:100px"></td>
-                    <td><input id="fileInput-${school["teacher-ID"]}" type="file" accept="image/*"></td>
-                </tr>`;
-            } catch (error) {
-                console.error('Error getting download URL:', error);
-            }  
         } else {
             tableHtml += `
             <tr>
@@ -117,36 +99,46 @@ async function generateTeacherTable() {
                 <td>${school["subject"]}</td>
                 <td>${school["email"]}</td>
                 <td>${school["address"]}</td>
-                <td><img id="previewImg-${school["teacher-ID"]}" src="../../picture/user.jpg" style="height:100px"></td>
+                <td style="text-align: center;">
+                    <img id="previewImg-${school["teacher-ID"]}" src="../../picture/user.jpg" style="height:100px">
+                    <button id="delFile-${school["teacher-ID"]}" style="display:none;"> Delete </button>
+                </td>
                 <td><input id="fileInput-${school["teacher-ID"]}" type="file" accept="image/*"></td>
             </tr>`;
         }    
     }
-
-
     tableHtml += `</table>`;
     document.getElementById("schoolTable").innerHTML = tableHtml;
 
     // เพิ่ม event listener ให้กับ input element ในแต่ละ row
     schoolData.forEach((school) => {
-        document.getElementById(`fileInput-${school["teacher-ID"]}`).addEventListener("change", function () {
-            handleImageUpload(this, school["teacher-ID"]);
-        });
+        const fileInput = document.getElementById(`fileInput-${school["teacher-ID"]}`);
+        if (fileInput) {
+            fileInput.addEventListener("change", function () {
+                handleImageUpload(this, school["teacher-ID"]);
+            });
+        }
+        
+        const delButtonPng = document.getElementById(`delFile-${school["teacher-ID"]}`);
+        if (delButtonPng) { 
+            delButtonPng.addEventListener("click", function () {
+                let fileName = `${school["teacher-ID"]}.png`;
+                handleDeleteImage(fileName);
+            });
+        } 
     });
 }
 
 async function handleImageUpload(input, teacherID) {
     const file = input.files[0];
-    if (!file) {
-        alert("Please select a file.");
+    if (!file.name.startsWith(teacherID)) {
+        alert("File name does not match teacher-ID");
         return;
     }
 
-    if (!file.name.startsWith(teacherID)) {
-        if (file.name.endsWith(".jpg") || file.name.endsWith(".png")) {
-            alert("File name does not match teacher-ID");
-            return;
-        }
+    if (!file.name.endsWith(".png")) {
+        alert("File must be a PNG image");
+        return;   
     }
 
     // สร้าง FileReader object เพื่ออ่านไฟล์รูปภาพ
@@ -154,18 +146,38 @@ async function handleImageUpload(input, teacherID) {
 
     // เมื่อไฟล์อ่านเสร็จสิ้นแล้ว
     reader.onload = function (e) {
-        // ให้แสดงภาพที่อ่านได้ใน tag <img> เพื่อแสดง preview
         const previewImg = document.getElementById(`previewImg-${teacherID}`);
         previewImg.src = e.target.result;
-    };
 
-    // อ่านไฟล์รูปภาพ
+        const delButton = document.getElementById(`delFile-${teacherID}`);
+        delButton.style.display = "inline";
+    };
     reader.readAsDataURL(file);
 
     const storageRef = ref(storage, `school/${currentUser.school_name}/teacher-images/${file.name}`);
     await uploadBytes(storageRef, file);
     console.log("Image uploaded successfully");
-    // alert("Image uploaded successfully");
+}
+
+
+async function handleDeleteImage(teacherID) {
+    console.log(`Delete image ${teacherID} . . .`);
+    const imageRef = ref(storage, `school/${currentUser.school_name}/teacher-images/${teacherID}`);
+
+    try {
+        await deleteObject(imageRef);
+        console.log(`Image ${teacherID} deleted successfully`);
+        alert("Image deleted successfully");
+
+        let [id, ext] = teacherID.split(".")
+        const previewImg = document.getElementById(`previewImg-${id}`);
+        if (previewImg) {
+            previewImg.src = "../../picture/user.jpg";
+        } 
+
+    } catch (error) {
+        console.error('Error deleting image:', error);
+    }
 }
 
 
